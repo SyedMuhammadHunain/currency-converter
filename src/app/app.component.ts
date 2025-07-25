df5e533d26162f62be10b291e3c74b24c8ff58da
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-07-25 16:18:56 +0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import { CurrencyComponent } from './currency/currency.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     HeaderComponent,
-    UserInputComponent,
-    CurrencyComponent,
+    UserInputComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
